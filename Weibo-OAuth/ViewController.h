0343c4bccc1239b71b9496fc9e5731b217aeff4d
Dwarven YANG <prison.yang@gmail.com>
2015-01-13 12:15:41 +0800

    init project

---
+++ Weibo-OAuth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weibo-OAuth
+//
+//  Created by 杨建亚 on 15/1/13.
+//  Copyright (c) 2015年 Dwarven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Weibo-OAuthTests/Weibo_OAuthTests.m
@@ -0,0 +1,40 @@
+//
+//  Weibo_OAuthTests.m
+//  Weibo-OAuthTests
+//
+//  Created by 杨建亚 on 15/1/13.
+//  Copyright (c) 2015年 Dwarven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Weibo_OAuthTests : XCTestCase
+
+@end
+
+@implementation Weibo_OAuthTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
